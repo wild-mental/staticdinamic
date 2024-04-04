dae0a37e0a20c2d54aa50f254af88d0dc4937fe7
wild-mental <wild.mental@kidsnote.com>
2024-04-05 05:57:52 +0900

    간단한 article 작성 form 과 데이터 인코딩

--- src/main/java/ac/syu/staticdinamic/SimpleRestController.java
+++ src/main/java/ac/syu/staticdinamic/SimpleRestController.java
@@ -1,6 +1,7 @@
 package ac.syu.staticdinamic;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,11 @@ public class SimpleRestController {
     @RequestMapping("/hello")
     public String hello() {
         return "hello world";
+    }
+
+    @RequestMapping("/article")
+    public String createArticle(@RequestParam("title") String title,
+                                @RequestParam("content") String content) {
+        return String.format("title: %s, content: %s", title, content);
     }
 }
